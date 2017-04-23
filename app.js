d013ee81921b4544113b2d17565b5d3b758ed577
Laura <laurachevalier4@yahoo.com>
2017-04-23 14:40:07 -0400

    I have a visual

--- app.js
+++ app.js
@@ -34,11 +34,6 @@ const Question = mongoose.model('Question');
 const Answer = mongoose.model('Answer');
 const ObjectId = mongoose.Types.ObjectId;
 
-// const pie = require('./public/js/pie.js');
-const d3 = require("d3");
-const jsdom = require("node-jsdom");
-const jsdoc = jsdom.jsdom();
-
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -90,73 +85,6 @@ hbs.registerHelper('userVoted', function(question) {
 hbs.registerHelper('pluralize', function(number, single, plural) {
   if (number === 1) { return single; }
   else { return plural; }
-});
-
-hbs.registerHelper('drawChart', function(question_id) {
-  var svg = d3.select(jsdoc.body).append("svg"); // individual svg has id #question_id
-  svg.attr("id", question_id);
-  console.log("in pie.js");
-  var width = +svg.attr("width"),
-      height = +svg.attr("height"),
-      radius = Math.min(width, height) / 2,
-      g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-  var color = d3.scaleOrdinal(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
-
-  var pie = d3.pie()
-      .sort(null)
-      .value(function(d) { return d.voters; });
-
-  var path = d3.arc()
-      .outerRadius(radius - 10)
-      .innerRadius(0);
-
-  var label = d3.arc()
-      .outerRadius(radius - 40)
-      .innerRadius(radius - 40);
-
-  // need to change this so it's not hardcoded as
-  var graph = d3.json("http://localhost:5000/api/" + question_id + "/voters.json", function(error, data) {
-    console.log("data in d3.json", data);
-    data = [data];
-    let f = function(d) {
-      d.answers = Object.keys(d);
-      d.answers = d.answers.map(function(ans) {
-        return {
-          "answer": ans,
-          "voters": d[ans].length
-        };
-      });
-      console.log("d.answers", d.answers);
-      return d;
-    }
-    console.log("in d3.json callback");
-    if (error) throw error;
-
-    var arc = g.selectAll(".arc")
-      .data(pie(data))
-      .enter().append("g")
-        .attr("class", "arc");
-
-    arc.append("path")
-        .attr("d", path)
-        .attr("fill", function(d) { return color(d.data.answer); });
-
-    arc.append("text")
-        .attr("transform", function(d) { return "translate(" + label.centroid(d) + ")"; })
-        .attr("dy", "0.35em")
-        .text(function(d) { return d.data.answer; });
-
-    console.log("svg html", svg.html());
-    return svg.html();
-  });
-  console.log(graph);
-  return graph;
-
-  // these lines get called before the json callback completes...
-  // how can I return svg.html() to the helper only AFTER d3.json() is done executing?
-  // thought I could use promises but couldn't figure out how to return svg to the helper function instead of the fulfill function of the Promise's .then()
-  // return svg;
 });
 
 app.get('/', function(req, res) {
@@ -449,6 +377,7 @@ app.get('/api/:question_id/voters.json', function(req, res) {
   }
 
   function doStuff(answers) {
+    console.log(answers);
     let userInfo = answers.map(function(ans) {
       return new Promise(function (fulfill, reject) {
         let text = ans.text;
--- public/js/pie.js
+++ public/js/pie.js
@@ -1,9 +1,7 @@
 function drawChart(question_id) {
   console.log(question_id);
-  var svg = d3.select(document.body).append("svg"); // individual svg has id #question_id
-  svg.attr("id", question_id);
-
-  console.log("in pie.js");
+  var svg = d3.select("#" + question_id); // individual svg has id #question_id
+  console.log(svg);
   var width = +svg.attr("width"),
       height = +svg.attr("height"),
       radius = Math.min(width, height) / 2,
@@ -13,7 +11,7 @@ function drawChart(question_id) {
 
   var pie = d3.pie()
       .sort(null)
-      .value(function(d) { return d.population; });
+      .value(function(d) { return d.voters; });
 
   var path = d3.arc()
       .outerRadius(radius - 10)
@@ -23,12 +21,22 @@ function drawChart(question_id) {
       .outerRadius(radius - 40)
       .innerRadius(radius - 40);
 
-  // need to change this so it's not hardcoded as
-  d3.json("http://localhost:5000/api/" + question_id + "/voters.json", function(d) {
-    console.log(d);
-    d.population = +d.population;
-    return d;
-  }, function(error, data) {
+  // need to change this so it's not hardcoded as localhost
+  d3.json("http://localhost:5000/api/" + question_id.slice(3) + "/voters.json", function(error, data) {
+    console.log("data in d3.json", data);
+    data = [data];
+    data.forEach(function(d) {
+      d.answers = Object.keys(d);
+      d.answers = d.answers.map(function(ans) {
+        return {
+          "answer": ans,
+          "voters": d[ans].length
+        };
+      });
+    });
+    data = data[0].answers;
+    console.log(data);
+    console.log("in d3.json callback");
     if (error) throw error;
 
     var arc = g.selectAll(".arc")
@@ -38,13 +46,12 @@ function drawChart(question_id) {
 
     arc.append("path")
         .attr("d", path)
-        .attr("fill", function(d) { return color(d.data.age); });
+        .attr("fill", function(d) { console.log(d); return color(d.data.answer); });
 
     arc.append("text")
         .attr("transform", function(d) { return "translate(" + label.centroid(d) + ")"; })
-        .attr("dy", "0.35em")
-        .text(function(d) { return d.data.age; });
+        .attr("dy", "0.1em")
+        .classed("charttext", true)
+        .text(function(d) { return d.data.answer; })
   });
-
-  return svg.html();
-};
+}
