06dcb6040b0ab99c8c641452b01fc60c99ab59b3
Laura <laurachevalier4@yahoo.com>
2017-04-27 17:10:47 -0400

    deleted unnecessary js files

--- public/js/custom.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-
-  $('.dropdown-toggle').hover(function() {
-    $(this).css('color', '#DDFFDD');
-  }, function() {
-    $(this).css('color', 'white');
-  });
-
-});
